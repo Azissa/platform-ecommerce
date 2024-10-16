cf67e553e4302e738cc9469a4e79c694e20e1520
Azissa <azisagni28@gmail.com>
2024-10-16 14:32:46 +0800

    create entity

--- server/src/entities/cart.ts
+++ server/src/entities/cart.ts
@@ -1,2 +1,50 @@
-import { Entity,PrimaryGeneratedColumn,Column,CreateDateColumn } from "typeorm";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  OneToMany,
+  ManyToOne,
+} from "typeorm";
 import { User } from "./user";
+import { Product } from "./product";
+import "reflect-metadata";
+
+@Entity()
+export class Cart {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @OneToMany(() => User, (user_id) => user_id.id)
+  user_id: User[];
+
+  @Column({ type: "int" })
+  total: number;
+
+  @CreateDateColumn({ type: "timestamp" })
+  created_at: Date;
+
+  @CreateDateColumn({ type: "timestamp" })
+  deleted_at: Date;
+}
+
+@Entity()
+export class CartItem {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @ManyToOne(() => Cart, (cart_id) => cart_id.id)
+  cart_id: Cart[];
+
+  @ManyToOne(() => Product, (product_id) => product_id.id)
+  product_id: Product[];
+
+  @Column({ type: "int" })
+  quantity: number;
+
+  @CreateDateColumn({ type: "timestamp" })
+  created_at: Date;
+
+  @CreateDateColumn({ type: "timestamp" })
+  deleted_at: Date;
+}
